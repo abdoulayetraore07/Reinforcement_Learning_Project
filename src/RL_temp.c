3686d722908641725d7398288ce64b533fdc5efa
abdoulayetraore07 <144810441+abdoulayetraore07@users.noreply.github.com>
2024-04-09 16:14:02 +0200

    Update RL_temp.c

--- src/RL_temp.c
+++ src/RL_temp.c
@@ -1,1 +1,14 @@
+#include "mazeEnv.h"
+#include "functions.h"
+
+char** mazeEnv;
+int** visited;
+int rows;
+int cols;
+int start_row;
+int start_col;
+int state_row;
+int state_col;
+int goal_row;
+int goal_col;
 
