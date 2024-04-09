44885063db4c032893e159d15080f639514454d4
abdoulayetraore07 <144810441+abdoulayetraore07@users.noreply.github.com>
2024-04-09 16:15:30 +0200

    Update RL_temp.c

--- src/RL_temp.c
+++ src/RL_temp.c
@@ -12,3 +12,39 @@ int state_col;
 int goal_row;
 int goal_col;
 
+
+void mazeEnv_make(char* file_name){
+     char c;
+     char rows_s[3] ={'\0'};
+     char cols_s[3] ={'\0'};
+     int rows_i = 0;
+     int cols_i = 0;
+     int swap = 0;
+
+     FILE* file = fopen(file_name, "r");
+     if (file == NULL){
+        printf("Pointeur nul");
+     }
+
+     if (file) {
+         /* lire la premiere ligne avant EOF */
+         while( (c=getc(file)) != EOF) {
+               if(c== '\n'){
+                      break;
+               } else if (c==',') {
+                      swap = 1;
+               } else if (!swap) {
+                      rows_s[rows_i]=c;
+                      rows_i++;
+               } else {
+                      cols_s[cols_i]= c;
+                      cols_i++;
+               }
+         }
+     }
+
+     /* convertir le string en nombre */
+     rows = atoi(rows_s);
+     cols = atoi(cols_s);
+
+     alloc_mazeEnv();
