7e088a9fc6bc7bbfcb4c5e3f986c0956ad5624e7
abdoulayetraore07 <144810441+abdoulayetraore07@users.noreply.github.com>
2024-04-30 00:40:52 +0200

    Create test.c

---
+++ data/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main ()  {
+
+Test BDD
+  
+1) Si état_t= i avec i € [ 0, n_états]
+   alors action € { ensemble etats } ;
+2) Si etat_t > n_etat
+  alors retourner erreur ;
+3) Si etat_t < 0 
+  alors retourner erreur ;
+return 0;
+
+
+
+Etant donné epsilon=0;
+  Quand  q(état, . )= [1,3,2,-1];
+  alors action = haut 
+
+Etant donné epsilon=1 ;
+  ...
+
+Etant donné Q(état, . )=[0, 0, 0, 0] ;
+  ...
+  
+}
