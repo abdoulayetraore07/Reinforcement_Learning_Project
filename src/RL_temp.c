462b879f0da52f9a060f3401693d5eb2d14320a9
abdoulayetraore07 <144810441+abdoulayetraore07@users.noreply.github.com>
2024-04-30 00:01:12 +0200

    Update RL_temp.c pour évaluation_mi-temps

--- src/RL_temp.c
+++ src/RL_temp.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "mazeEnv.h"
 #include "functions.h"
 #include <math.h>
@@ -21,17 +22,18 @@ double gamma_perso=0.1;
 int** Q;
 double epsilon=0.2;
 char police ;
+int done = 0 ;
 
 
-void alloc_mazeEnv(){
-     mazeEnv = malloc(rows * sizeof(char*));
+void alloc_mazeEnv(){                                           /* Creation du labyrinthe */
+     mazeEnv = malloc(rows * sizeof(char*));  
 
      for(int i=0; i<rows; i++) {
          mazeEnv[i] = malloc(cols * sizeof(char*));
      }
 }
 
-void mazeEnv_make(char* file_name){
+void mazeEnv_make(char* file_name){                             /* Fonction de creation du labyrinthe à partir du nom du fichier */
      char c;
      char rows_s[3] ={'\0'}; 
      char cols_s[3] ={'\0'};
@@ -88,13 +90,12 @@ void mazeEnv_make(char* file_name){
      fclose(file);
 }
 
-
-void mazeEnv_reset(){             
+void mazeEnv_reset(){                                           /* Fonction d'initialisation de la cellule courante avec la cellule de depart */
      state_row = start_row;
      state_col = start_col;
      }
 
-void alloc_visited()
+void alloc_visited()                                            /* Initialisation des différents terrains rencontrés pendant l'exploration */
 {
         visited = malloc(rows * sizeof(int*));
         int i;
@@ -103,7 +104,7 @@ void alloc_visited()
         }
 }
 
-void init_visited()
+void init_visited()                                             /* Fonction d'initialisation des différents terrains rencontrés pendant l'exploration */
 {
         alloc_visited();
 
@@ -121,15 +122,14 @@ void init_visited()
         }
 }
 
-
-double rewarder (int x1, int y1, int x2, int y2 ) {   
+double rewarder (int x1, int y1, int x2, int y2 ) {             /* Fonction de récompense en fonction de la distance eucludienne */
      int reward_max=10000;
      double norme=sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) ) ;
      double rewards = (1/norme+1)*10000 + reward_max*0.25 ;   
      return rewards;
 }
 
-int min (int a, int b) {
+int min (int a, int b) {                                        /* Fonction min */
      if (a>b) {
           return b;
      } else {
@@ -137,7 +137,7 @@ int min (int a, int b) {
      }
 }
 
-int max (int a, int b) {
+int max (int a, int b) {                                        /* Fonction max */
      if (a>b) {
           return a;
      } else {
@@ -145,9 +145,8 @@ int max (int a, int b) {
      }
 }
 
-envOutput mazeEnv_step(action a){
+envOutput mazeEnv_step(action a){                               /* Fonction d'attribution de la récompense et du nouvel état */
     int rewards = 0;
-    int done = 0;
     envOutput stepOut;
     int state_row_new=state_row;
     int state_col_new=state_col;
@@ -166,10 +165,10 @@ envOutput mazeEnv_step(action a){
         rewards = -1000000; 
         state_row_new=state_row;
         state_col_new=state_col; 
-        printf("Mur\n");
+        printf("Mur\n\n");
    } else {
         rewards = rewarder (state_row_new,state_col_new,goal_row,goal_col) ;
-        printf("Non Mur\n");
+        printf("Non Mur\n\n");
    }
      
     if((state_row == goal_row) && (state_col == goal_col)){
@@ -184,13 +183,14 @@ envOutput mazeEnv_step(action a){
    return stepOut;
 }
 
-struct policy {
+struct policy {                                                 /* Struct pour retourner la valeur de Q_max et et l'action choisie grace aux polices */
      double Q_max;
      enum action current_act;
 };
 
 
-struct policy choice_policy_eps(int state_row,int state_col) {  /* Permettant de retourner Q_max et action correspondant */
+struct policy choice_policy_eps(int state_row,int state_col) {  /* Fonction choice_policy par epsilon_greedy */
+
      double Q_max= Q[state_row*cols + state_col][0];
      enum action current_act = up ;
      for ( int j=1; j<nb_actions; ++j ) {    
@@ -202,7 +202,7 @@ struct policy choice_policy_eps(int state_row,int state_col) {  /* Permettant de
      int alea_1=rand() % 100 ;
      int borne= 100*epsilon ;
      if (alea_1<borne) {
-          int alea_2= rand()%3 ;
+          int alea_2= rand()% 4 ;
           current_act= (enum action) (alea_2) ;
           Q_max= Q[state_row*cols + state_col][current_act]; 
      }
@@ -212,16 +212,16 @@ struct policy choice_policy_eps(int state_row,int state_col) {  /* Permettant de
 
 }
 
-struct policy choice_policy_bolt(int state_row,int state_col) {  /* Permettant de retourner Q_max et action correspondant */
+struct policy choice_policy_bolt(int state_row,int state_col) { /* Fonction choice_policy par exploration de boltzman */
      
      double somme_expo = 0;
      for (int j=0; j< nb_actions ; ++j ) {
           somme_expo+= exp(Q[state_row*cols + state_col][j]);
      }
-     double proba_up = exp(Q[state_row*cols + state_col][0]) / somme_expo ;
-     double proba_down = exp(Q[state_row*cols + state_col][1]) / somme_expo ;
-     double proba_left = exp(Q[state_row*cols + state_col][2]) / somme_expo ;
-     double proba_right = exp(Q[state_row*cols + state_col][3]) / somme_expo ;
+     double proba_up = exp(Q[state_row*cols + state_col][0]) / somme_expo ;       // Probabilité de choisir action up ;
+     double proba_down = exp(Q[state_row*cols + state_col][1]) / somme_expo ;     // Probabilité de choisir action down ;
+     double proba_left = exp(Q[state_row*cols + state_col][2]) / somme_expo ;     // Probabilité de choisir action left ;
+     double proba_right = exp(Q[state_row*cols + state_col][3]) / somme_expo ;    // Probabilité de choisir action right ;
 
      int alea=rand() % 100 ;
      double Q_act = 0;
@@ -243,6 +243,8 @@ struct policy choice_policy_bolt(int state_row,int state_col) {  /* Permettant d
                     current_act = right ;
                     Q_act= Q[state_row*cols + state_col][current_act] ;    
                }
+          }
+     }
                
      struct policy retour= {Q_act,current_act} ;
      return retour;
@@ -250,7 +252,7 @@ struct policy choice_policy_bolt(int state_row,int state_col) {  /* Permettant d
 }
 
 
-void mazeEnv_render_pos(){
+void mazeEnv_render_pos(){                                      /* Fonction d'affichage du labyrinthe, de l'état actuel et de la trajectoire */
      mazeEnv[state_row][state_col] = 'o';
      for (int i=0; i<rows; i++) {
          for (int j=0; j< cols; j++){
@@ -264,73 +266,71 @@ void mazeEnv_render_pos(){
 
 int main( int argc, char* argv[] ) {
      
-     srand( time( NULL ) );
+     srand( time( NULL ) );                                     
 
-     mazeEnv_make("maze.txt");        /* Creation du labyrinthe */
+     mazeEnv_make("maze.txt");                                  /* Creation du labyrinthe */
      nblignes_Q = rows*cols ;
 
-     Q = malloc(nblignes_Q * sizeof(double));        /* Creation du tableau Q */
+     Q = malloc(nblignes_Q * sizeof(double));                   /* Creation du tableau Q */
      for(int i=0; i<nblignes_Q; i++) {
          Q[i] = malloc(nb_actions * sizeof(double));
      }
      
-     for (int i=0; i<nblignes_Q; ++i ) {   /* Initialisation du tableau Q */
+     for (int i=0; i<nblignes_Q; ++i ) {                        /* Initialisation du tableau Q */
           for ( int j=0; j<nb_actions; ++j ) {
-              int temp = rand() % (2*nblignes_Q) ;
+              int temp = rand() % (2*nblignes_Q) ;              /* Nombre aléatoire pour remplir Q */
               Q[i][j]= temp-nblignes_Q ;
           }     
      }    
   
-     int start_row_Q= start_row*cols + start_col ;  /*  Numero cellule du start dans le labyrinthe */
-     int goal_row_Q= goal_row*cols + goal_col ;  /*  Numero cellule du goal dans le labyrinthe */
+     int start_row_Q= start_row*cols + start_col ;              /*  Numero cellule du start dans le labyrinthe */
+     int goal_row_Q= goal_row*cols + goal_col ;                 /*  Numero cellule du goal dans le labyrinthe */
     
-     for (int j=0; j<nb_actions; ++j )  {    /* Initialisation du tableau Q dans le cas terminal */
+     for (int j=0; j<nb_actions; ++j )  {                       /* Initialisation du tableau Q dans le cas terminal */
           Q[goal_row_Q][j]=0;  
      }
 
-     init_visited() ;    /* Initialisation du tableau visited */
-          
+     init_visited() ;                                           /* Initialisation du tableau visited */
+
+
+
       /*** Partie training ***/
 
 
-     mazeEnv_reset();               /*Initialiser la cellule courante avec la cellule de depart */
+     mazeEnv_reset();                                           /* Initialiser la cellule courante avec la cellule de depart */
+       
 
-     int choix_police = 0 ;    // Choix de la police pour l'exploration entre epsilon_greedy et Boltzman ;
-     do { 
-          printf("Entrez le chiffre 1 pour la police epsilon_greedy ou 2 pour l'exploration de Boltzman");
-          scanf("%d",&choix_police);
-     } while ( choix_police != 1 && choix_police != 2 ) ;
+    clock_t start, end; 
+    double elapsed; 
+  
+    start = clock();                                            /* Lancement de la mesure pour connaître le délai d'éxecution de la boucle */
 
-     if ( choix_police == 1 ) {
-          police = "eps" ;
-     } else { 
-           police = "bolt" ;
-     }
-          
-     while (( state_row != goal_row || state_col != goal_col) ) {
-          printf("______________________________________________");
+     while ( done != 1 ) {
+          printf("-----------------------------------------------\n\n\n");
           mazeEnv_render_pos()  ;   /*Affichage de l'etat actuel */
 
-          struct policy state = choice_policy_police(state_row,state_col) ; /* Choix de l'action telle que Q est maximum  */
+          struct policy state = choice_policy_bolt(state_row,state_col) ; /* Choix de l'action en fonction de la police et de Q pour l'état courant */
           envOutput stepOut=mazeEnv_step(state.current_act) ;   /* Observation rewards and new_state */
           
-          double rewards = stepOut.reward ;
-          int state_row_new = stepOut.new_row  ;          
+          double rewards = stepOut.reward ;                     /* Récuperation de la récompense */
+          int state_row_new = stepOut.new_row  ;                /* Récuperation du nouvel_état */
           int state_col_new = stepOut.new_col ;
+          double Q_max=state.Q_max ;                            /* Récuperation de la valeur de Q pour l'action choisie précedemment pour l'état courant */
 
-          struct policy state_new = choice_policy_police(state_row_new,state_col_new);  /* Choix de l'action telle que Q est maximum pour le nouvel état  */
-          double Q_max=state.Q_max ;
-          double Q_max_new=state_new.Q_max ;
+          struct policy state_new = choice_policy_bolt(state_row_new,state_col_new);  /* Choix de l'action en fonction de la police et de Q pour le nouvel état  */
+          
+          double Q_max_new=state_new.Q_max ;                    /* Récuperation de la valeur de Q pour l'action choisie précedemment pour le nouvel état */
          
-          Q[state_row*cols + state_col][state.current_act] +=  alpha*( rewards + gamma_perso*Q_max_new - Q_max ) ;
+          Q[state_row*cols + state_col][state.current_act] +=  alpha*( rewards + gamma_perso*Q_max_new - Q_max ) ;  /* Modification de la valeur de Q */
           state_row= state_row_new ;
           state_col= state_col_new ;
        
      }
 
-     return 0;
-     
+     end = clock();                                    /* Arrêt de la mesure     */ 
+     elapsed = ((double)end - start) / CLOCKS_PER_SEC; /* Conversion en secondes  */      
+
+     printf("Bravo, vous avez atteint la sortie en : %.2f secondes. \n\n", elapsed ) ;
+
      
 }
-    
-
