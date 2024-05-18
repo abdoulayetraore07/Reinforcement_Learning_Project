7182d60ca879d46ada39423e148c53610cbffec1
abdoulayetraore07 <144810441+abdoulayetraore07@users.noreply.github.com>
2024-05-18 03:44:46 +0200

    Update functions.c

--- src/functions.c
+++ src/functions.c
@@ -1,9 +1,9 @@
 #include "functions.h"
 
-double min(double a, double b){
+int min(int a, int b){
     return (a>b) ? b : a;
 }
 
-double max(double a, double b){
+int max(int a, int b){
     return (a>b) ? a : b;
 }
